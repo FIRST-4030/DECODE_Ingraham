b7fa1416ff3b0c70fdc0756db18c1b3e0df7c84a
finley <finley.page13@gmail.com>
2025-10-10 17:07:25 -0700

    Added Obelisk Detection

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/shootertest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/shootertest.java
@@ -38,6 +38,9 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
 
+import java.util.List;
+
+// Modified BasicOpMode_Linear.java by Finley
 /*
  * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
  * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
@@ -58,6 +61,7 @@ public class shootertest extends LinearOpMode {
     // Declare OpMode members.
     // private ElapsedTime runtime = new ElapsedTime();
     private DcMotor shooter = null;
+    private static double rangeAprilTag = 0;
     // private DcMotor rightDrive = null;
 
     @Override
@@ -68,44 +72,31 @@ public class shootertest extends LinearOpMode {
         // Initialize the hardware variables. Note that the strings used here as parameters
         // to 'get' must correspond to the names assigned during the robot configuration
         // step (using the FTC Robot Controller app on the phone).
-        shooter  = hardwareMap.get(DcMotor.class, "shooter");
-        // rightDrive = hardwareMap.get(DcMotor.class, "right_drive");
+        shooter = hardwareMap.get(DcMotor.class, "shooter");
 
         // To drive forward, most robots need the motor on one side to be reversed, because the axles point in opposite directions.
         // Pushing the left stick forward MUST make robot go forward. So adjust these two lines based on your first test drive.
         // Note: The settings here assume direct drive on left and right wheels.  Gear Reduction or 90 Deg drives may require direction flips
-        shooter.setDirection(DcMotor.Direction.REVERSE);
-        // rightDrive.setDirection(DcMotor.Direction.FORWARD);
-
+        shooter.setDirection(DcMotor.Direction.FORWARD);
+        // shooter.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         // Wait for the game to start (driver presses START)
         waitForStart();
         // runtime.reset();
 
+        // Setup a variable for each drive wheel to save power level for telemetry
+        double shooterPower;
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
-
-            // Setup a variable for each drive wheel to save power level for telemetry
-            double shooterPower;
-            // Choose to drive using either Tank Mode, or POV Mode
-            // Comment out the method that's not used.  The default below is POV.
-
-            // POV Mode uses left stick to go forward, and right stick to turn.
-            // - This uses basic math to combine motions and is easier to drive straight.
-            // double drive = -gamepad1.left_stick_y;
-            // double turn  =  gamepad1.right_stick_x;
-            shooterPower    = 0.1; // Range.clip(drive + turn, -1.0, 1.0) ;
-
-            // Tank Mode uses one stick to control each wheel.
-            // - This requires no math, but it is hard to drive forward slowly and keep straight.
-            // shooterPower  = -gamepad1.left_stick_y ;
-            // rightPower = -gamepad1.right_stick_y ;
-            // Send calculated power to wheels
+            shooterPower = rangeAprilTag; // Range.clip(drive + turn, -1.0, 1.0)
             shooter.setPower(shooterPower);
 
             // Show the elapsed game time and wheel power.
             // telemetry.addData("Status", "Run Time: " + runtime.toString());
             telemetry.addData("Motors", "shooter (%.2f)", shooterPower);
             telemetry.update();
+
         }
+
     }
+
 }

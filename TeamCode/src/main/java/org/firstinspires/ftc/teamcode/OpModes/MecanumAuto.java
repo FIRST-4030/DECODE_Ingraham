b9dca324d647456eaa4590136eff60a1e69ebc64
gitaccount123456 <moment348090@gmail.com>
2025-10-11 11:29:04 -0700

    Made Linear Auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/MecanumAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/MecanumAuto.java
@@ -1,12 +1,13 @@
 package org.firstinspires.ftc.teamcode.OpModes;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 @Autonomous
-public class MecanumAuto extends OpMode
+public class MecanumAuto extends LinearOpMode
 {
     // Declare OpMode members.
     private ElapsedTime runtime    = new ElapsedTime();
@@ -16,7 +17,7 @@ public class MecanumAuto extends OpMode
     private int allianceSide;
 
     @Override
-    public void init() {
+    public void RunOpMode() {
         /*
          * Code that runs ONCE when the driver hits INIT
          *    Tasks to include are,
@@ -33,50 +34,10 @@ public class MecanumAuto extends OpMode
         // Tell the driver that initialization is complete.
         telemetry.addData("Status", "Initialized");
         telemetry.update();
-    }
+        waitForStart();
 
-    @Override
-    public void init_loop() {
-        /*
-         * Code that runs REPEATEDLY after the driver hits INIT, but before you hit START
-         *    Tasks to include are,
-         *       - Allow driver to set the alliance color
-         */
-        if(gamepad1.x){
-            allianceSide = 20;
-        }
-        else if(gamepad1.b){
-            allianceSide = 24;
-        }
-        telemetry.addData("Alliance Side:",allianceSide);
-        telemetry.update();
-    }
 
-    @Override
-    public void start() {
-        /*
-         * Code that runs ONCE when the driver hits START
-         *    Tasks to include are,
-         *       - Initialize April tags
-         *       - Initialize any time parameters
-         */
-        runtime.reset();
-    }
 
-    @Override
-    public void loop() {
-        /*
-         * Code that runs continuously throughout the entire autonomous period
-         */
-    }
 
-    @Override
-    public void stop() {
-        /*
-         * Code to run ONCE after the driver hits STOP
-         *    Tasks to include are,
-         *       - Stop motors
-         *       - Close cameras
-         */
     }
 }
